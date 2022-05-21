c761a9d4b58cdba2d9b6538d769a4f08609b603e
Mlkey9 <m.mustin9@protonmail.com>
2022-05-21 15:06:19 -0400

    correct solution for boolean-to-string

---
+++ boolean-to-string.js
@@ -0,0 +1,22 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+// P: given a boolean, return boolean as a string
+// R: return? console.log?
+
+function booleanToString(b) {
+  if (b === true) {
+    return "true";
+  } else if (b === false) {
+    return "false";
+  }
+}
+
+// E:
+console.log(true), "true";
+console.log(false), "false";
+
+// P: create a function a that takes in a boolean as a parameter.
+// convert the boolean to a String
+// return the string
